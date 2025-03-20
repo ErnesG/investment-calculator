05c44f62dbd07cf575b0dad8afb2571b30f05b00
LATEAM99 <egonzl92@outlook.com>
2025-03-19 19:53:35 -0700

    removes reactiveForms features, uses basic formsmodule

--- src/app/components/user-input/user-input.component.ts
+++ src/app/components/user-input/user-input.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
@@ -6,6 +7,15 @@ import { Component } from '@angular/core';
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
-export class UserInputComponent {
+export class UserInputComponent{
+    annualInvestment: number = 0;
+    duration: number = 0;
+    expectedReturn: number = 0;
+    initialInvestment: number = 0;
+   
+  onSubmit() {
+    
+  }
+    
 
 }
