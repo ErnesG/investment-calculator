1adee2e8a61d8eb784a228ba177041389fc6d393
LATEAM99 <egonzl92@outlook.com>
2025-03-19 20:38:44 -0700

    adds dependency for currency piping

--- src/app/components/user-input/user-input.module.ts
+++ src/app/components/user-input/user-input.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from "@angular/core";
 import { UserInputComponent } from "./user-input.component";
 import { ResultsComponent } from "./results/results.component";
 import { FormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
 
 @NgModule({
     declarations: [UserInputComponent, ResultsComponent],
     exports: [
         UserInputComponent
     ],
-    imports: [FormsModule]
+    imports: [FormsModule, CommonModule]
 
 
 })
