6d05fabb2a147b31d421b427d8bdb33126a53820
LATEAM99 <egonzl92@outlook.com>
2025-03-19 20:01:42 -0700

    on submit logic

--- src/app/components/user-input/user-input.component.ts
+++ src/app/components/user-input/user-input.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { InvestmentService } from '../../services/investment.service';
+import { Investment } from '../../model/investment.model';
 
 @Component({
   selector: 'app-user-input',
@@ -12,9 +14,14 @@ export class UserInputComponent{
     duration: number = 0;
     expectedReturn: number = 0;
     initialInvestment: number = 0;
-   
+    investmentService = inject(InvestmentService)
+    showResults = false
+    private results!: Investment[]
   onSubmit() {
-    
+    if(this.annualInvestment > 0 && this.duration > 0 && this.expectedReturn > 0 && this.initialInvestment>0) {
+        this.results = this.investmentService.calculateInvestmentResults(this.annualInvestment, this.duration, this.expectedReturn, this.initialInvestment);
+        this.showResults = true;
+    }
   }
     
 
